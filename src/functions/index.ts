259a08a1b3d078b8bef1297029ddde2f1700e997
Urbani Simone <urb.simone@gmail.com>
2021-05-25 18:40:35 +0200

    EsLint fix

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,5 +1,5 @@
-export { default as scanTag } from '@functions/scan/'
-export { default as getTag } from '@functions/get/'
-export { default as updateTag } from '@functions/update/'
-export { default as deleteTag } from '@functions/delete/'
-export { default as createTag } from '@functions/create/'
+export { default as scanTag } from '@functions/scan/';
+export { default as getTag } from '@functions/get/';
+export { default as updateTag } from '@functions/update/';
+export { default as deleteTag } from '@functions/delete/';
+export { default as createTag } from '@functions/create/';

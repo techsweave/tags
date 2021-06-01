155769d9400961c59d5d5ea5bb7ea98fc63775e2
Urbani Simone <urb.simone@gmail.com>
2021-06-01 17:32:31 +0200

    Get tag test

--- tests/get/handlerTest.ts
+++ tests/get/handlerTest.ts
@@ -44,6 +44,6 @@ describe('handler: getTag', async () => {
 
         const body = JSON.parse(response.body);
 
-        expect(body.error.name, 'body.name').to.be.equal('ItemNotFoundException');
+        expect(body.error.name, 'body.error.name').to.be.equal('ItemNotFoundException');
     })
 })

43803a43c6ea290b75f4593c2fd430bea42ea027
Urbani Simone <urb.simone@gmail.com>
2021-06-04 16:51:10 +0200

    Eslint fix

--- tests/scan/handlerTest.ts
+++ tests/scan/handlerTest.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
-import { main as scanTagHandler } from '../../src/functions/scan/handler'
-import { fakeContext, IFakeEvent, Models } from 'utilities-techsweave'
+import { main as scanTagHandler } from '../../src/functions/scan/handler';
+import { fakeContext, IFakeEvent, Models } from 'utilities-techsweave';
 // import Tag from '../../src/models/database/tables/tags'
 
 function instanceOfITag(object: any): object is Models.Tables.ITag {
@@ -17,9 +17,9 @@ describe('handler: scanTag', async () => {
             body: {
                 limit: 2
             }
-        }
+        };
 
-        let response = await scanTagHandler(event, fakeContext);
+        const response = await scanTagHandler(event, fakeContext);
 
         expect(response).to.be.not.null;
         expect(response.statusCode, 'statusCode').to.be.equal(200);
@@ -28,10 +28,10 @@ describe('handler: scanTag', async () => {
 
         expect(body.count, 'body.count').to.be.equal(2);
 
-        expect(body.data).to.be.a('Array')
+        expect(body.data).to.be.a('Array');
         expect(instanceOfITag(body.data[0])).to.be.true;
         expect(body.lastEvaluatedKey.id).to.be.a('string');
-    })
+    });
 
     it('Should return a response whit a set of tags after a specified tag', async () => {
 
@@ -40,9 +40,9 @@ describe('handler: scanTag', async () => {
                 limit: 25,
                 startKey: 'c04ce116-dcf4-4e7f-9e9b-3f3e97e0fecc'
             }
-        }
+        };
 
-        let response = await scanTagHandler(event, fakeContext);
+        const response = await scanTagHandler(event, fakeContext);
 
         expect(response).to.be.not.null;
         expect(response.statusCode, 'statusCode').to.be.equal(200);
@@ -51,8 +51,8 @@ describe('handler: scanTag', async () => {
 
         expect(body.count, 'body.count').to.be.greaterThanOrEqual(1);
 
-        expect(body.data).to.be.a('Array')
+        expect(body.data).to.be.a('Array');
         expect(instanceOfITag(body.data[0])).to.be.true;
         expect(body.lastEvaluatedKey).to.not.exist;
-    })
-})
+    });
+});
